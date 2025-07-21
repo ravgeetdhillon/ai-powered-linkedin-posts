ad2db9d28e4948ce6f54b900b08c2b474ee84112
Ravgeet Dhillon <ravgeetdhillon@gmail.com>
2025-07-21 19:51:17 +0530

    feat: added LinkedIn tag

--- src/main.py
+++ src/main.py
@@ -254,6 +254,7 @@ def add_post_to_notion(title, topic, date):
             "Due Date": {"date": {"start": date}},
             "Status": {"select": {"name": "To Do"}},
             "Type": {"select": {"name": "Marketing"}},
+            "Tags": {"multi_select": ["name": "LinkedIn"]},
         },
         "children": [
             {
